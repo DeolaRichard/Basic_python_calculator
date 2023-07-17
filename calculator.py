3a122a8461cdfbf7fa96813b6b5d35cf1f1be2a8
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-17 18:08:23 +0100

    Wrote codes for basic math functions

---
+++ calculator.py
@@ -0,0 +1,25 @@
+def add(a, b):
+    answer = a + b
+    print(str(a) + " + " + str(b) + " = " + str(answer))
+
+
+def sub(a, b):
+    answer = a - b
+    print(str(a) + " - " + str(b) + " = " + str(answer))
+
+
+def mul(a, b):
+    answer = a * b
+    print(str(a) + " * " + str(b) + " = " + str(answer))
+
+
+def div(a, b):
+    answer = a / b
+    print(str(a) + " / " + str(b) + " = " + str(answer))
+
+
+print("A. Addition")
+print("B. Subtraction")
+print("C. Multiplication")
+print("D. Division")
+
