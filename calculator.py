0ee6b135ab145dbd2730e9cbbfa1f9a970d2956d
DeolaRichard <adekunlerichie5@gmail.com>
2023-07-17 18:32:57 +0100

    Added block of codes to decipher user's choice of math function & also accept user's input.

--- calculator.py
+++ calculator.py
@@ -25,6 +25,25 @@ print("D. Division")
 
 choice = input("input your choice: ")
 
-if choice == "a" or choice =="A":
+if choice == "a" or choice == "A":
     print("Addition")
-    a = int(input("Enter first number"))
+    a = int(input("Enter first number:"))
+    b = int(input("Enter second number:"))
+    add(a, b)
+elif choice == "b" or choice == "B":
+    print("Subtraction")
+    a = int(input("Enter first number:"))
+    b = int(input("Enter second number:"))
+    sub(a, b)
+elif choice == "c" or choice == "C":
+    print("Multiplication")
+    a = int(input("Enter first number:"))
+    b = int(input("Enter second number:"))
+    mul(a, b)
+elif choice == "d" or choice == "D":
+    print("Division")
+    a = int(input("Enter first number:"))
+    b = int(input("Enter second number:"))
+    div(a, b)
+
+
